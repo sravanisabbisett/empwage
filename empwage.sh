37674d30f0389b6ae783dc598c4b5c3a8babdf4a
sravanisabbisett <sravani.sabbisetti1@gmail.com>
2020-08-05 15:11:19 +0530

    uc5 added

--- empwage.sh
+++ empwage.sh
@@ -1,17 +1,22 @@
 #!/bin/bash -x
-empRatePerHour=20
+dailyWagePerHrs=20
+numofDays=20
 isFullTime=2
 isPartTime=1
-randomCheck=$(( RANDOM%3 ))
+for ((days=1; days<=$numofDays; days++))
+do
+	randomCheck=$(( RANDOM%3 ))
+	case $randomCheck in
+			$isFullTime)
+					empHrs=8 ;;
+			$isPartTime)
+					empHrs=4 ;;
+			*)
+					empHrs=0 ;;
+	esac
 
-case $randomCheck in
-					$isFullTime)
-									empHrs=8 ;;
-					$isPartTime)
-									empHrs=4 ;;
-					*)
-									empHrs=0 ;;
-esac
-
-salaray=$(( $empRatePerHour*$empHrs ))
-echo $salary
+	salary=$(( $dailyWagePerHrs*$empHrs ))
+	echo $salary
+	totalsalary=$(( $totalsalary+$salary ))
+	echo $totalsalary
+done
